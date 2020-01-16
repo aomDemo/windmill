0ff0e563776134b4e0e9f022820dbfc025440cc0
David Jordan <jordan@apiomat.com>
2020-01-16 10:13:04 +0100

    [F, TP-10] add comments for backend

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -7,6 +7,7 @@ const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent,
+    // TODO: uncomment to check if user can go to this page
     // canActivate: [AuthGuard]
   }
 ];
